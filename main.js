80b5761768fe69f3609cc7bc963e8205c6cbf1eb
Shiyu Wang <shiyu3169@gmail.com>
2019-04-18 15:42:10 -0400

    change into es6

--- main.js
+++ main.js
@@ -1,40 +1,32 @@
-// adding an event listener for page load
-window.addEventListener("load", fetchBookmarks);
 
-// Find the form and add an event listener to it
-document.querySelector("form").addEventListener("submit", saveBookmark);
 
 // Save Bookmarks
-function saveBookmark(e) {
+const saveBookmark = e => {
   // Prevent from page reloading
   e.preventDefault();
 
   // Get site name and site url
-  var siteName = document.querySelector("#siteName").value;
-  var siteUrl = document.querySelector("#siteUrl").value;
+  const siteName = document.querySelector("#siteName").value;
+  const siteUrl = document.querySelector("#siteUrl").value;
   
   // Create a bookmark object
-  var bookmark = {
+  const bookmark = {
     name: siteName,
     url: siteUrl
   }
 
+  let bookmarks = [];
+
   // Check if the local storage is empty
-  if(localStorage.getItem("bookmarks") === null) {
-    // Init bookmarks array
-    var bookmarks = [];
-    // Adding first bookmark
-    bookmarks.push(bookmark);
-    // Set bookmarks to localstorage
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-  } else {
+  if(localStorage.getItem("bookmarks") !== null) {
     // Get current bookmarks from local storage
-    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
-    // Push new bookmark into bookmarks
-    bookmarks.push(bookmark);
-    // set bookmarks to localstorage
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
   }
+
+   // Adding first bookmark
+   bookmarks.push(bookmark);
+   // Set bookmarks to localstorage
+   localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 
   // Reset the form
   document.querySelector("form").reset();
@@ -44,37 +36,37 @@ function saveBookmark(e) {
 }
 
 // Fetch bookmarks
-function fetchBookmarks() {
+const fetchBookmarks = () => {
   // Get bookmarks from local storage
-  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
 
   // Select the bookmarks div
-  var output = document.querySelector("#bookmarks");
+  const output = document.querySelector("#bookmarks");
 
   // Reset the bookmarks div
   output.innerHTML = "";
 
   // Loop over bookmarks
-  for(var i=0;i<bookmarks.length;i++) {
+  for(let bookmark of bookmarks) {
     // Create div
-    var div = document.createElement("div");
+    const div = document.createElement("div");
     // Create h3
-    var h3 = document.createElement("h3");
-    h3.textContent = bookmarks[i].name;
+    const h3 = document.createElement("h3");
+    h3.textContent = bookmark.name;
 
     // Create visit link
-    var a = document.createElement("a");
-    a.href = bookmarks[i].url;
+    const a = document.createElement("a");
+    a.href = bookmark.url;
     a.className = "btn btn-success";
     a.textContent = "Visit"
 
     // Create delete button
-    var button = document.createElement("button");
+    const button = document.createElement("button");
     button.className = "btn btn-danger";
     button.textContent = "Delete";
     
-    button.addEventListener("click", function(e){
-        var name = e.target.parentElement.children[0].textContent;
+    button.addEventListener("click", e => {
+        const name = e.target.parentElement.children[0].textContent;
         deleteBookmark(name);
     })
 
@@ -89,12 +81,12 @@ function fetchBookmarks() {
   }
 }
 
-function deleteBookmark(name) {
+const deleteBookmark = name => {
     // Get bookmarks from localStorage;
     var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
 
     // Loop over bookmarks
-    for(var i=0; i<bookmarks.length;i++) {
+    for(let i in bookmarks) {
       // looking for bookmark by given name
       if(bookmarks[i].name === name) {
         bookmarks.splice(i, 1);
@@ -108,3 +100,9 @@ function deleteBookmark(name) {
     // Re-fetch bookmarks output
     fetchBookmarks();
 }
+
+// Find the form and add an event listener to it
+document.querySelector("form").addEventListener("submit", saveBookmark);
+
+// adding an event listener for page load
+window.addEventListener("load", fetchBookmarks);
